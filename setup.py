528a8496cb5a5e768d43c472be4104316ea8d308
Dale Gaines <dalegainesii@gmail.com>
2023-08-17 01:04:20 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup
 setup(
     name="vasp_manager",
     version="0.1.0",
-    description="A simple package to run and analyze vasp calculations",
+    description="A simple package to run and analyze VASP calculations",
     packages=["vasp_manager"],
     url="https://github.com/dgaines2/vasp_manager",
     author="Dale Gaines II",
     author_email="dalegainesii@gmail.com",
     license="MIT",
     include_package_data=True,
+    python_requires=">=3.10.0",
     install_requires=[
         "numpy>=1.22",
         "pandas>=1.4",
