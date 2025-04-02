0c91b958bf917087e95bbb69c7f847782407b0f2
Dale Gaines <dalegainesii@gmail.com>
2025-04-02 15:30:28 -0500

    v1.3.1

--- vasp_manager/__init__.py
+++ vasp_manager/__init__.py
@@ -5,7 +5,7 @@ A python package to run and analyze VASP calculations
 """
 
 __package__ = "vasp_manager"
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 __author__ = "Dale Gaines II"
 
 import logging
